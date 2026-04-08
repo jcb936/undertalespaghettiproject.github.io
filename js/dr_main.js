c1f7fa6f247e39e3f4a7f9d71d964c6f45ec1bc2
renard <jacob.arbib@gmail.com>
2026-04-08 14:50:34 +0100

    Some website cleanup: - Refactored JS code into own file - Started removing JQuery - Added cachebusting - Prettier-ified

---
+++ js/dr_main.js
@@ -0,0 +1,67 @@
+import { handlePageScroll, handleTopBar } from "./common.js";
+
+function handleAppearingElements() {
+  var viewportHeight = $(window).height();
+  window.addEventListener("scroll", throttle(callback, 400));
+
+  function callback() {
+    $(window).scroll(function () {
+      var scroll = $(window).scrollTop();
+      var viewportHeight = $(window).height();
+
+      if (scroll >= viewportHeight * 0.1) {
+        $(".title1").addClass("moved-title1");
+        $(".description1").addClass("moved-description1");
+        $(".top-bar").addClass("visible-top-bar");
+      }
+
+      if (scroll < viewportHeight * 0.4) {
+        $(".top-bar").removeClass("visible-top-bar");
+      }
+
+      if (scroll >= viewportHeight * 1.7) {
+        $(".title2").addClass("moved-title2");
+        $(".slider-immagini").addClass("moved-slider-immagini");
+      }
+
+      if (scroll >= viewportHeight * 1.8) {
+        $(".title3").addClass("moved-title3");
+        $(".download-icon").addClass("animated-download-icon");
+        $(".download-button").addClass("animated-download-button");
+      }
+
+      if (scroll >= viewportHeight * 1.5) {
+        $(".title4").addClass("moved-title4");
+        $(".social-fb").addClass("moved-social-fb");
+        $(".social-discord").addClass("moved-social-discord");
+        $(".social-bluesky").addClass("moved-social-bluesky");
+        $(".social-email").addClass("moved-social-email");
+      }
+
+      if (scroll >= viewportHeight * 1.0) {
+        $(".title5").addClass("moved-title5");
+        $(".staff-listRune").addClass("animated-staff-list");
+        $(".staff-avatar").addClass("animated-staff-avatar");
+      }
+    });
+  }
+
+  function throttle(callback, limit) {
+    var wait = false;
+    return function () {
+      if (!wait) {
+        callback.call();
+        wait = true;
+        setTimeout(function () {
+          wait = false;
+        }, limit);
+      }
+    };
+  }
+}
+
+// Do the JS - Should be done after DOMContentLoaded (e.g. loaded by the Cachebuster)
+
+handleAppearingElements();
+handlePageScroll();
+handleTopBar();
